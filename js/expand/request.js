27c2df54c3771e82b976c2e954ac4fffc4ef9315
“gaowei1012” <mingwei2222@126.com>
2020-08-11 16:39:48 +0800

    完成用户注册登录

--- js/expand/request.js
+++ js/expand/request.js
@@ -3,12 +3,13 @@ import constant from './api';
 
 const { base_url } = constant
 
-export function request(url, method) {
+export function request(url, method, data) {
     return new Promise((resolve, reject) => {
         axios({
             url: url,
             baseURL: base_url,
             method: method,
+            data: data
         })
             .then(res => {
                 resolve(res.data);
--- js/views/Personal/Login.js
+++ js/views/Personal/Login.js
@@ -3,20 +3,55 @@ import { View, StyleSheet, Text, SafeAreaView, TextInput, TouchableOpacity } fro
 import { px2dp } from '../../utils/px2dp'
 import { GoBack } from '../../utils/GoBack'
 import TopNavigationBar from '../../common/TopNavigationBar'
+import { connect } from 'react-redux'
+import actions from './redux/action'
+import constant from '../../expand/api'
 
-export default class Login extends React.PureComponent {
+const { register } = constant
+
+class Login extends React.PureComponent {
     state = {
         account: null,
-        password: null
+        password: null,
+        newPassword: null,
+        register: true, // 注册
+        registerAccount: null,
+        registerPassword: null
     }
-    handleChangeAccout = (account) => {
+    handleLoginAccout = (account) => {
         this.setState({ account })
     }
-    handleChangePassword = (password) => {
+    handleLoginPassword = (password) => {
         this.setState({ password })
     }
-    handleSubmit=()=> {
-        // 登录
+    handleRegisterNewPassword = (newPassword) => {
+        this.setState({ newPassword })
+    }
+    handleRegisterPassword = (registerPassword) => {
+        this.setState({ registerPassword })
+    }
+    handleRegisterAccout = (registerAccount) => {
+        this.setState({ registerAccount })
+    }
+    /* 提交注册 */
+    _register = () => {
+        const { getRegister } = this.props
+        const { registerAccount, registerPassword, newPassword } = this.state
+        if (registerPassword !== newPassword) {
+            return <Text>密码不匹配</Text>
+        } else {
+            const data = {
+                "username": registerAccount,
+                "password": registerPassword
+            }
+            getRegister(register, 'POST', data)
+        }
+    }
+    // 切换到注册
+    _switch = () => {
+        this.setState({
+            register: false
+        })
     }
     render() {
         const StatusBar = {
@@ -31,41 +66,79 @@ export default class Login extends React.PureComponent {
             />
         );
         const _title = (
-            <Text style={styles.loginTitle}>账号密码登录</Text>
+            <>
+                {this.state.register ? <Text style={styles.loginTitle}>账号密码登录</Text> : <Text style={styles.loginTitle}>用户注册</Text>}
+            </>
         );
         const _content = (
-            <View style={styles.textInputBox}>
-                <TextInput
-                    style={styles.textInput}
-                    placeholder={'请输入账号'}
-                    onChangeText={this.handleChangeAccout}
-                />
-                <TextInput
-                    style={styles.textInput}
-                    placeholder={'请输入密码'}
-                    onChangeText={this.handleChangePassword}
-                    secureTextEntry={true}
-                />
-            </View>
+            <>
+                {this.state.register ? <View style={styles.textInputBox}>
+                    <TextInput
+                        style={styles.textInput}
+                        placeholder={'请输入账号'}
+                        onChangeText={this.handleLoginAccout}
+                    />
+                    <TextInput
+                        style={styles.textInput}
+                        placeholder={'请输入密码'}
+                        onChangeText={this.handleLoginPassword}
+                        secureTextEntry={true}
+                    />
+                </View> : <View style={styles.textInputBox}>
+                        <TextInput
+                            style={styles.textInput}
+                            placeholder={'请输入账号'}
+                            onChangeText={this.handleRegisterAccout}
+                        />
+                        <TextInput
+                            style={styles.textInput}
+                            placeholder={'请输入密码'}
+                            onChangeText={this.handleRegisterPassword}
+                            secureTextEntry={true}
+                        />
+                        <TextInput
+                            style={styles.textInput}
+                            placeholder={'请再次输入密码'}
+                            onChangeText={this.handleRegisterNewPassword}
+                            secureTextEntry={true}
+                        />
+                    </View>}
+            </>
         );
         const _password = (
-            <TouchableOpacity
-                activeOpacity={1}
-                onPress={this.handleSubmit}
-                style={styles.password}
-            >
-                <Text style={styles.passwordText}>忘记密码？</Text>
-            </TouchableOpacity>
+            <>
+                {this.state.register ? <View style={styles.footerBox}>
+                    <TouchableOpacity
+                        activeOpacity={1}
+                        onPress={this._switch}
+                    >
+                        <Text>注册</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity
+                        activeOpacity={1}
+                        onPress={this.handleSubmit}
+                        style={styles.password}
+                    >
+                        <Text style={styles.passwordText}>忘记密码？</Text>
+                    </TouchableOpacity>
+                </View> : null}
+            </>
         );
         const _footerBtn = (
             <>
-                <TouchableOpacity
+                {this.state.register ? <TouchableOpacity
                     onPress={this._submit}
                     activeOpacity={1}
                     style={styles.submitBox}
                 >
                     <Text style={styles.submitText}>登录</Text>
-                </TouchableOpacity>
+                </TouchableOpacity> : <TouchableOpacity
+                    onPress={this._register}
+                    activeOpacity={1}
+                    style={styles.submitBox}
+                >
+                        <Text style={styles.submitText}>注册</Text>
+                    </TouchableOpacity>}
                 {_password}
             </>
 
@@ -81,6 +154,14 @@ export default class Login extends React.PureComponent {
         )
     }
 }
+
+export default connect(({ register }) => ({
+    register
+}), dispatch => ({
+    getRegister(url, method, data) {
+        dispatch(actions.getRegister(url, method, data))
+    }
+}))(Login)
 
 const styles = StyleSheet.create({
     loginBox: {
@@ -120,12 +201,21 @@ const styles = StyleSheet.create({
         fontWeight: '600'
     },
     password: {
-        marginTop: px2dp(6),
-        width: px2dp(335),
-        alignSelf: 'center',
-        alignItems: 'flex-end',
+        // marginTop: px2dp(6),
+        // width: px2dp(335),
+        // alignSelf: 'center',
+        // alignItems: 'flex-end',
     },
     passwordText: {
         color: '#333'
+    },
+    footerBox: {
+        width: px2dp(335),
+        marginTop: px2dp(6),
+        flexDirection: 'row',
+        alignItems: 'center',
+        alignSelf: 'center',
+        paddingHorizontal: px2dp(6),
+        justifyContent: 'space-between'
     }
 })
