2e2aeffbf92f00fc6595e87e3e4074ff00c255d3
“gaowei1012” <mingwei2222@126.com>
2020-08-17 19:27:50 +0800

    添加接口

--- js/expand/api.js
+++ js/expand/api.js
@@ -1,7 +1,13 @@
 const constant = {
     base_url: 'http://127.0.0.1:5062',
     register: '/api/user/register',
-    login: '/api/user/login'
+    login: '/api/user/login',
+    shop: '/api/shops/findShopByType',
+    allOrder: '/api/order/getAllOrder',
+    addOnesOrder: '/api/order/addOrderOnes',
+    orderById: '/api/order/getOrderById',
+    delRrderById: '/api/order/deleteOrderById',
+    address: '/api/address/getAddress'
 }
 
 export default constant
