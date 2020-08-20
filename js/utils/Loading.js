abeb37fe8b50682bb6eeb449699ff50a80d18844
“gaowei1012” <mingwei2222@126.com>
2020-08-20 13:42:03 +0800

    fix loading

--- js/utils/Loading.js
+++ js/utils/Loading.js
@@ -1,17 +1,18 @@
 import React from 'react'
 import RootSilblings from 'react-native-root-siblings'
-import { View, StyleSheet, ActivityIndicator } from 'react-native'
+import { View, StyleSheet, ActivityIndicator, Text } from 'react-native'
 
-import {width, height} from '../utils/px2dp'
+import {width, height, px2dp} from '../utils/px2dp'
 
 let sibling = undefined
 
 const Loading = {
-    show: () => {
+    show: (msg) => {
         sibling = new RootSilblings(
             <View style={styles.maskStyle}>
                 <View style={styles.backViewStyle}>
                     <ActivityIndicator size='large' color='white' />
+                    <Text style={styles.msg}>{msg}</Text>
                 </View>
             </View>
         )
@@ -30,7 +31,7 @@ export {
 const styles = StyleSheet.create({
     maskStyle: {
         position: 'absolute',
-        backgroundColor: 'rgba(0, 0, 0, 0.3)',
+        backgroundColor: 'rgba(0, 0, 0, 0)',
         width: width,
         height: height,
         alignItems: 'center',
@@ -43,5 +44,10 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
         alignItems: 'center',
         borderRadius: 5,
+    },
+    msg: {
+        color: '#fff',
+        fontSize: px2dp(12),
+        marginTop: px2dp(6)
     }
 })
